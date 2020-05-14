86144dd33f1c3d9fac859d6772ef046566860f15
yma67 <35381478+yma67@users.noreply.github.com>
2020-05-13 22:48:33 -0400

    Enforcing ANRL coding style/best practice

--- src/core/scheduler/context.c
+++ src/core/scheduler/context.c
@@ -39,7 +39,7 @@
 void makecontext(jam_ucontext_t *ucp, void (*func)(void), int argc, ...) {
 	va_list va;
 	memset(ucp->registers, 0, 16 * 8);
-	if(argc != 2) *(int*)0 = 0;
+	if(argc != 2) __builtin_trap();
 	va_start(va, argc);
 	ucp->registers[14] = va_arg(va, int);
 	ucp->registers[15] = va_arg(va, int);
