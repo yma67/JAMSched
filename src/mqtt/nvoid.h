e64a87121103d1da2eb773e1aac0d8a3108bec88
anrl <mahes25@gmail.com>
2020-06-07 01:08:50 -0400

    initial mqtt adapter

---
+++ src/mqtt/nvoid.h
@@ -0,0 +1,91 @@
+/**
+ * @file        nvoid.h
+ * @brief       Memory block management functions
+ * @details     Can store a memory segment pointed to by a void pointer in this block, binary data is accepted here. 
+ * @author      Muthucumaru Maheswaran, Yuxiang Ma
+ * @copyright 
+ *              Copyright 2020 Muthucumaru Maheswaran, Yuxiang Ma
+ * 
+ *              Licensed under the Apache License, Version 2.0 (the "License");
+ *              you may not use this file except in compliance with the License.
+ *              You may obtain a copy of the License at
+ * 
+ *                  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *              Unless required by applicable law or agreed to in writing, software
+ *              distributed under the License is distributed on an "AS IS" BASIS,
+ *              WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *              See the License for the specific language governing permissions and
+ *              limitations under the License.
+ */
+#ifndef NVOID_H
+#define NVOID_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+ * @struct nvoid_t
+ * @brief  a block of memory and the length of memory to replace strings. 
+ */
+typedef struct nvoid_t {
+    int len;                                            /// length of the memory block 
+    void* data;                                         /// pointer to actual memory block - we could have done without this pointer
+} nvoid_t;
+
+/**
+ * Memory block creator
+ * @param data: data that initializes the memory block
+ * @param len: length of the data (size of the memory block)
+ * @warning DO NOT USE a NULL in the data field 
+ * @see    use nvoid_null() to create NULL memory block
+ * @return NULL if not possible to allocate memory, or memory block, otherwise
+ */
+nvoid_t *nvoid_new(void *data, int len);
+
+/**
+ * NULL Memory block creator
+ * @see use nvoid_new() to create a memory block with the given initializers
+ * @return NULL if not possible to allocate memory, or memory block, otherwise
+ */
+nvoid_t *nvoid_null();
+
+/**
+ * Memory block destroyer
+ * @param ptr: pointer to the memory block that needs destroying (deallocation)
+ */
+void nvoid_free(nvoid_t *ptr);
+
+/**
+ * Append to a memory block
+ * @param ptr: old memory block
+ * @param data: data to the appended to the old block
+ * @param len: length of the data to be appended
+  * @return NULL if not possible to allocate memory, or memory block, otherwise
+ */
+nvoid_t *nvoid_append(nvoid_t *ptr, void *data, int len);
+
+/**
+ * Concatanate two memory blocks
+ * @param first: first memory block
+ * @param second: second memory block
+ * @warning the second memory block is not released by nvoid_concat() function
+ * @return NULL if not possible to allocate memory for the larger block, or memory block, otherwise
+ */
+nvoid_t *nvoid_concat(nvoid_t *first, nvoid_t *second);
+
+/**
+ * Print a memory block
+ * @param ptr: memory block
+ */
+void nvoid_print(nvoid_t *ptr);
+
+/**
+ * Print a memory block in ASCII
+ * @param ptr: memory block
+ */
+void nvoid_print_ascii(nvoid_t *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/mqtt/testers/mqtt_tester.c
@@ -0,0 +1,62 @@
+#include "../mqtt_adapter.h"
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/*
+ * Callback invoked when the client connects to the 
+ * MQTT server.
+ */
+void connected(void *a) {
+    // a is a pointer to a mqtt_adapter_t structure.
+    printf("Connected to ... mqtt... \n");
+}
+
+int msg_arrived(void *ctx, char *topicname, int topiclen, MQTTAsync_message *msg)
+{
+   
+    printf("Topic name: %s\n", topicname);
+    printf("Message %s\n", (char *)msg->payload);
+    // msg is byte array pointed to by payload and has length payloadlen 
+
+    MQTTAsync_freeMessage(&msg);
+    MQTTAsync_free(topicname);
+    return 1;
+}
+
+int main(int argc, char *argv[]) 
+{
+    nvoid_t *nv;
+    char buffer[200];
+
+    if (argc != 2) {
+        printf("Usage: mqtt-tester client|server\n\n");
+        exit(1);
+    }
+    mqtt_adapter_t *mq = mqtt_createserver("tcp://localhost:1883", 1, "app-1", "dev-2", connected);
+    if (strcmp(argv[1], "client") == 0) {
+        mqtt_connect(mq);        
+        for (int j = 0; j < 5000000; j++) 
+        {   
+            sprintf(buffer, "Hello World-%d", j);
+            nv = nvoid_new(buffer, strlen(buffer));
+
+            if (mqtt_publish(mq, "/testing-abc", nv) == false) 
+                printf("Publishing failed \n");
+            sleep(1);        
+        }
+    } else {
+        mqtt_setmsgarrived(mq, msg_arrived);
+        mqtt_connect(mq);
+        mqtt_set_subscription(mq, "/testing-abc");
+    }
+
+    if (strcmp(argv[1], "server") ==0)
+        sleep(1000);
+    else 
+        sleep(100);
+    mqtt_deleteserver(mq);
+    sleep(1);
+}
