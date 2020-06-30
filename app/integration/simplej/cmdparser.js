ba3ea8f6632fdb3dfd03ece4fd5409267b163fef
yma67 <35381478+yma67@users.noreply.github.com>
2020-06-30 02:07:30 -0400

    New SimpleJ Node

--- app/integration/simplej/cmdparser.js
+++ app/integration/simplej/cmdparser.js
@@ -1,15 +1,15 @@
 const commandLineArgs = require('command-line-args');
 
-// simpleje  --port(p) number  --command(c) cmd --topic(t) topic
+// simpleje  --port(p) number  --command(c) cmd --app(a) appname
 // port defaults to 1883
-// command defaults to: send "async", "sync" is the other option
+// command defaults to: "none"
 // topic defaults to "/mach/func/request"
 module.exports = new function parseArgs() {
 
     const optdefs = [
         { name: 'port', alias: 'p', type: String, defaultValue: '1883'},
-        { name: 'command', alias: 'c', type: String, defaultValue: 'async'},       
-        { name: 'topic', alias: 't', type: String, defaultValue: '/app-1/mach/func/request'}                
+        { name: 'command', alias: 'c', type: String, defaultValue: 'none'},       
+        { name: 'app', alias: 'a', type: String, defaultValue: 'app-1'}
     ];
     var options;
     try {
